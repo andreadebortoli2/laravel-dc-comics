2b401787a4dce2312e9d5b0c9ff2e67620dac0b3
andreadebortoli2 <debortoliandrea88@gmail.com>
2024-05-15 15:07:09 +0200

    add layouts setup

---
+++ resources/views/comics/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('page-title', 'COMICS')
+
+@section('content')
+    <div>
+        comics
+    </div>
+@endsection
---
+++ resources/views/comics/show.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('page-title', 'COMIC')
+
+@section('content')
+    <div>
+        comic
+    </div>
+@endsection
