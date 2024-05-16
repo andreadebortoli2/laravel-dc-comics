110d2bb8bce2b48d2f703e0bbd63d26d9e099236
andreadebortoli2 <debortoliandrea88@gmail.com>
2024-05-16 14:37:41 +0200

    move alert list to partials.error-alert

--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -4,15 +4,7 @@
 
 @section('content')
 
-    @if ($errors->any())
-        <div class="alert alert-danger">
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
+    @include('partials.error-alert')
 
     <form action="{{ route('comics.update', $comic) }}" method="post">
         @csrf
